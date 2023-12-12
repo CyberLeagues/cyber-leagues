28b8f11e7a3e7df2fd68cf514ea5bd718090e7b3
Paweł <s24803@pjwstk.edu.pl>
2023-12-12 18:35:10 +0100

    inicjalization commit

---
+++ cyber-leagues-module/src/main/java/pl/cyberleagues/cyberleaguesmodule/CyberLeaguesModuleApplication.java
@@ -0,0 +1,13 @@
+package pl.cyberleagues.cyberleaguesmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberLeaguesModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberLeaguesModuleApplication.class, args);
+    }
+
+}
---
+++ cyber-leagues-module/src/test/java/pl/cyberleagues/cyberleaguesmodule/CyberLeaguesModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cyberleagues.cyberleaguesmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberLeaguesModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
