982d497f7335fdcd7a07c332bffc9cc42496bc8b
Paweł <s24803@pjwstk.edu.pl>
2023-12-13 22:27:39 +0100

    add oauth2 login

---
+++ cyber-leagues-module/src/main/java/pl/cyberleagues/cyberleaguesmodule/configuration/SecurityConfig.java
@@ -0,0 +1,33 @@
+package pl.cyberleagues.cyberleaguesmodule.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/").permitAll();
+                    authorize.anyRequest().authenticated();
+                })
+                .formLogin(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults())
+                .oidcLogout();
+
+        http.logout(logout -> logout
+                .logoutSuccessUrl("/")
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID"));
+
+        return http.build();
+    }
+}
