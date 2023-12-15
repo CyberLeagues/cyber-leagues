c9ab4525c6951967e03e1c6d4ee2e068a53efed1
Paweł <s24803@pjwstk.edu.pl>
2023-12-15 19:03:26 +0100

    add game controller,service

--- cyber-leagues-module/src/main/java/pl/cyberleagues/cyberleaguesmodule/repositories/GameRepository.java
+++ cyber-leagues-module/src/main/java/pl/cyberleagues/cyberleaguesmodule/repositories/GameRepository.java
@@ -1,7 +1,9 @@
 package pl.cyberleagues.cyberleaguesmodule.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.cyberleagues.cyberleaguesmodule.models.Game;
 
+@Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
 }
