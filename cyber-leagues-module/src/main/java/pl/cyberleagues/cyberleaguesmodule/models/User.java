1ed3e741747786093af7c08b1a6c253a35db02e4
Paweł Pepliński <92047270+s24803@users.noreply.github.com>
2024-01-11 18:54:38 +0100

    Feature/save logged user to db (#14)
    * save user to db
    
    * save user to database
    
    * change datasource url

--- cyber-leagues-module/src/main/java/pl/cyberleagues/cyberleaguesmodule/models/User.java
+++ cyber-leagues-module/src/main/java/pl/cyberleagues/cyberleaguesmodule/models/User.java
@@ -1,31 +1,48 @@
 package pl.cyberleagues.cyberleaguesmodule.models;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
 @Entity
-@Table(name = "User")
-@Setter
-@Getter
-public class User {
+@Table(name = "users")
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "Name")
-    private String name;
+    @OneToMany
+    private Set<Authority> authorities;
 
-    @Column(name = "Email")
-    private String email;
-
-    @Column(name = "Password")
     private String password;
 
-    @ManyToOne
-    @JoinColumn(name = "Team_Id", nullable = false)
-    private Team team;
+    private String name;
 
+    private String picture;
+
+    @Column(unique = true)
+    private String username;
+
+    private boolean accountNonExpired;
+
+    private boolean accountNonLocked;
+
+    private boolean credentialsNonExpired;
+
+    private String provider;
+
+    private String providerId;
+
+    private boolean enabled;
 
 }
--- cyber-leagues-module/src/main/java/pl/cyberleagues/cyberleaguesmodule/repositories/UserRepository.java
+++ cyber-leagues-module/src/main/java/pl/cyberleagues/cyberleaguesmodule/repositories/UserRepository.java
@@ -1,10 +1,15 @@
 package pl.cyberleagues.cyberleaguesmodule.repositories;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.cyberleagues.cyberleaguesmodule.models.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @EntityGraph(attributePaths = "authorities")
+    Optional<User> findByUsername(String username);
 }
