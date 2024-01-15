ab3fbb5a6220f7d124570a18cc5df9a8ef73540e
KacperKnitter <knitterkacper@gmail.com>
2024-01-15 17:21:50 +0100

    added repositories for match invitation and registration. edited league repository.

---
+++ cyber-leagues-module/src/main/java/pl/cyberleagues/cyberleaguesmodule/repositories/InvitationRepository.java
@@ -0,0 +1,8 @@
+package pl.cyberleagues.cyberleaguesmodule.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.cyberleagues.cyberleaguesmodule.models.Invitation;
+@Repository
+public interface InvitationRepository extends JpaRepository<Invitation,Long>{
+}
---
+++ cyber-leagues-module/src/main/java/pl/cyberleagues/cyberleaguesmodule/repositories/MatchRepository.java
@@ -0,0 +1,10 @@
+package pl.cyberleagues.cyberleaguesmodule.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.cyberleagues.cyberleaguesmodule.models.Match;
+
+
+@Repository
+public interface MatchRepository extends JpaRepository<Match,Long> {
+}
