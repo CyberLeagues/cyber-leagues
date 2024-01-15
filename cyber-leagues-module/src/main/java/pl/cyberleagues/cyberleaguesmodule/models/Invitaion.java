777fed2289e6667f4b279a577655dbdefc8b6d03
KacperKnitter <88160078+KacperKnitter@users.noreply.github.com>
2024-01-15 18:55:42 +0100

    added models of invitation and registration (#17)

---
+++ cyber-leagues-module/src/main/java/pl/cyberleagues/cyberleaguesmodule/models/Invitaion.java
@@ -0,0 +1,20 @@
+package pl.cyberleagues.cyberleaguesmodule.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Invitaion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+}
---
+++ cyber-leagues-module/src/main/java/pl/cyberleagues/cyberleaguesmodule/models/Registration.java
@@ -0,0 +1,22 @@
+package pl.cyberleagues.cyberleaguesmodule.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Registration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "league_id")
+    private League league;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+}
+
+
