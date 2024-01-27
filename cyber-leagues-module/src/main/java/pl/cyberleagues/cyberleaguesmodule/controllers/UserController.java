d17454c835c135c8d7df5e3655431b209c7a60d0
KacperKnitter <88160078+KacperKnitter@users.noreply.github.com>
2024-01-27 20:36:10 +0100

    add user page (#19)
    * add user page
    
    * added user page

--- cyber-leagues-module/src/main/java/pl/cyberleagues/cyberleaguesmodule/controllers/UserController.java
+++ cyber-leagues-module/src/main/java/pl/cyberleagues/cyberleaguesmodule/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.cyberleagues.cyberleaguesmodule.models.League;
@@ -22,11 +23,18 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
 
-    private final UserService service;
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public String getUserById(Model model, @PathVariable Long id) {
+        User user = userService.getUserById(id);
+        model.addAttribute("user", user);
+        return "userTemplates/user";
+    }
 
     @GetMapping("/ownedLeagues")
-    public String getOwnedLeagues(Principal principal, Model model){
-        User user = service.getUserByProviderId(principal.getName());
+    public String getOwnedLeagues(Principal principal, Model model) {
+        User user = userService.getUserByProviderId(principal.getName());
         model.addAttribute("ownedLeagues", user.getOwnedLeagues());
 
         return "leagueTemplates/manager";
