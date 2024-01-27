0915743f0e2a395f070ac9907d3c7fbb2b608c63
KacperKnitter <88160078+KacperKnitter@users.noreply.github.com>
2024-01-27 23:46:23 +0100

    added team view page (#23)

--- cyber-leagues-module/src/main/java/pl/cyberleagues/cyberleaguesmodule/services/TeamService.java
+++ cyber-leagues-module/src/main/java/pl/cyberleagues/cyberleaguesmodule/services/TeamService.java
@@ -4,27 +4,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.cyberleagues.cyberleaguesmodule.listobjectfolder.ListHolderContainer;
 import pl.cyberleagues.cyberleaguesmodule.models.Team;
+import pl.cyberleagues.cyberleaguesmodule.repositories.TeamRepository;
+
+import java.util.Optional;
 import java.util.Set;
 
 
 @Service
+@RequiredArgsConstructor
 public class TeamService {
 
+    private final TeamRepository teamRepository;
 
-//    private final Set<Team> teamSet = ListHolderContainer.setofTeam;
-
-    /**
-     *  ze strony z ktorej bedziemy przechodzic do team, znajduje sie obiekt Game
-     *  ktory przechowuje tez id teamu, za pomoca tego id bedziemy przechodzic do strony z podanym teamem
-     *  wieec: przeszukaj liste i znajdz taki team ktorego id jest rowne temu co przyszlo ze strony,
-     *  jesli nie znajdzie to null,
-     * @param teamID
-     * @return
-     */
-
-//    public Team findTeamById(Long teamID)
-//    {
-//        return teamSet.stream().filter(team -> team.getId().equals(teamID)).findFirst().orElse(null);
-//    }
+    public Team getTeamById(Long id) {
+       Optional<Team> team = teamRepository.findById(id);
+       if (team.isPresent()){
+           return team.get();
+       }
+       return null;
+    }
 
 }
